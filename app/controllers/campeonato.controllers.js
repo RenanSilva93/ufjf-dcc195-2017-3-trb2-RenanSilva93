b9a775e9e7027d66a71c4bb1e350c11ce4dbad16
RenanSilva93 <renan.costas@hotmail.com>
2017-12-03 20:22:48 -0200

    Modificados os arquivos para resolver algumas mudanças de: jogo da velha, amigo oculto e campeonato

--- app/controllers/campeonato.controllers.js
+++ app/controllers/campeonato.controllers.js
@@ -16,8 +16,10 @@ module.exports.campeonato = function(req,res,next){
 		  j++;
 		}
 		
-		res.render("campeonato/formato", {equipes: times});
+		res.render("campeonato/formato", {equipes: times, fase: 8});
 	} else if(total == 7) {
+		var timesOitavas = req.body.timeAntigo;
+		var timesOitavasAntigo = []
 		var times = []
 		var timeGanhador = []
 		var j=0
@@ -35,8 +37,17 @@ module.exports.campeonato = function(req,res,next){
 		  times[k]={ 'timeMandante': timeGanhador[i],'timeVisitante': timeGanhador[i+1]}
 		  k++;
 		}
-		res.render("campeonato/formato", {equipes: times});
+		var n=0
+		for(var i=0;i<16;i=i+2){
+		  timesOitavasAntigo[n]={ 'timeMandante': timesOitavas[i],'timeVisitante': timesOitavas[i+1]}
+		  n++;
+		}
+		res.render("campeonato/formato", {equipes: times, fase: 4, equipesOitavas: timesOitavasAntigo});
 	} else if(total == 3) {
+		var timesOitavas = req.body.timesOitavasAntigo;
+		var timesOitavasAntigo = []
+		var timesQuartas = req.body.timeAntigo;
+		var timesQuartasAntigo = []
 		var times = []
 		var timeGanhador = []
 		var j=0
@@ -57,8 +68,26 @@ module.exports.campeonato = function(req,res,next){
 		  k++;
 		}
 		
-		res.render("campeonato/formato", {equipes: times});
+		var c=0
+		for(var c=0;i<16;i=i+2){
+		  timesOitavasAntigo[c]={ 'timeMandante': timesOitavas[i],'timeVisitante': timesOitavas[i+1]}
+		  c++;
+		}
+		
+		var n=0
+		for(var i=0;i<8;i=i+2){
+		  timesQuartasAntigo[n]={ 'timeMandante': timesQuartas[i],'timeVisitante': timesQuartas[i+1]}
+		  n++;
+		}
+		
+		res.render("campeonato/formato", {equipes: times, fase: 2, equipesOitavas: timesOitavasAntigo, equipesQuartas: timesQuartasAntigo});
 	} else if(total == 1) {
+		var timesOitavas = req.body.timesOitavasAntigo;
+		var timesOitavasAntigo = []
+		var timesQuartas = req.body.timesQuartasAntigo;
+		var timesQuartasAntigo = []
+		var timesSemi = req.body.timeAntigo;
+		var timesSemiAntigo = []
 		var times = []
 		var timeGanhador = []
 		var j=0
@@ -79,8 +108,34 @@ module.exports.campeonato = function(req,res,next){
 		  k++;
 		}
 		
-		res.render("campeonato/formato", {equipes: times});
+		var c=0
+		for(var c=0;i<16;i=i+2){
+		  timesOitavasAntigo[c]={ 'timeMandante': timesOitavas[i],'timeVisitante': timesOitavas[i+1]}
+		  c++;
+		}
+		
+		var n=0
+		for(var i=0;i<8;i=i+2){
+		  timesQuartasAntigo[n]={ 'timeMandante': timesQuartas[i],'timeVisitante': timesQuartas[i+1]}
+		  n++;
+		}
+		
+		var l=0
+		for(var i=0;i<4;i=i+2){
+		  timesSemiAntigo[l]={ 'timeMandante': timesSemi[i],'timeVisitante': timesSemi[i+1]}
+		  l++;
+		}
+		
+		res.render("campeonato/formato", {equipes: times, fase: 1, equipesOitavas: timesOitavasAntigo, equipesQuartas: timesQuartasAntigo, equipesSemi: timesSemiAntigo});
 	} else {
+		var timesOitavas = req.body.timesOitavasAntigo;
+		var timesOitavasAntigo = []
+		var timesQuartas = req.body.timesQuartasAntigo;
+		var timesQuartasAntigo = []
+		var timesSemi = req.body.timesSemiAntigo;
+		var timesSemiAntigo = []
+		var timesFinal = req.body.timesAntigo;
+		var timesFinalAntigo = []
 		var rand = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
 			var timeGanhador = []
 			var i = 0;
@@ -92,7 +147,28 @@ module.exports.campeonato = function(req,res,next){
 		
 		var times = [];
 		times[0]={ 'campeao': timeGanhador[0]}
-		res.render("campeonato/campeao", {equipes: times});
+		
+		var c=0
+		for(var c=0;i<16;i=i+2){
+		  timesOitavasAntigo[c]={ 'timeMandante': timesOitavas[i],'timeVisitante': timesOitavas[i+1]}
+		  c++;
+		}
+		
+		var n=0
+		for(var i=0;i<8;i=i+2){
+		  timesQuartasAntigo[n]={ 'timeMandante': timesQuartas[i],'timeVisitante': timesQuartas[i+1]}
+		  n++;
+		}
+		
+		var l=0
+		for(var i=0;i<4;i=i+2){
+		  timesSemiAntigo[l]={ 'timeMandante': timesSemi[i],'timeVisitante': timesSemi[i+1]}
+		  l++;
+		}
+		
+		timesFinalAntigo[0]={ 'timeMandante': time[0],'timeVisitante': time[1]}
+
+		res.render("campeonato/formato", {equipesCampeao: times, fase: 0, equipesOitavas: timesOitavasAntigo, equipesQuartas: timesQuartasAntigo, equipesSemi: timesSemiAntigo, equipesFinal: timesFinalAntigo});
 	} 
   }
 }
